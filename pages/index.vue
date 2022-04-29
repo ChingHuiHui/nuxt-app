bfe1ffce466c3bb8297bbf8735d0f8a432b18ca9
chinghuihui <sunny70624@gmail.com>
2022-04-29 21:31:03 +0800

    modify the index page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mt-20">
+    <h2 class="mb-5">用來測試 nuxt3 相關的環境</h2>
+
+    <ol class="list-decimal pl-5">
+      <li v-for="{ to, label } in links" :key="to">
+        <NuxtLink :to="to">{{ label }}</NuxtLink>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script setup>
+const links = [{ to: '/vuelidate', label: 'vuelidate' }]
+</script>
