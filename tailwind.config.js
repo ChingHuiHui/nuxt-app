c14b8438639b318841e2825f17da76850b363821
chinghuihui <sunny70624@gmail.com>
2022-04-29 20:56:52 +0800

    set up for TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './assets/css/**/*.css',
+    './components/**/*.vue',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
